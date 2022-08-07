e6872c3d3cfdf4767a83c92684a15ba6148a31e1
admin <admin@mail.loc>
2022-08-07 18:07:00 +0200

    added Add to cart button

--- themes/textiler/assets/js/app.js
+++ themes/textiler/assets/js/app.js
@@ -141,6 +141,30 @@ if (toggleConfirmPassword) {
         }
     });
 }
+
+// Event add product to cart
+$(document).on('click', '.btn-add-to-cart', (e) => {
+    const button = $(e.currentTarget),
+        form = button.parents('.product-wrapper');
+    let data = {
+        'cart': [
+            {
+                'offer_id': form.find('input[name="offer_id"]').val(),
+                'quantity': form.find('input[name="quantity"]').val(),
+            },
+        ]
+    };
+    addOfferToCart(form, button, data);
+});
+
+// Add product to cart
+function addOfferToCart(form, button, data) {
+    $.request('Cart::onAdd', {
+        'data': data,
+        'update': {'cart/count': '.header-cart'},
+    });
+}
+
 // Modal menu by Ajax, show categorys.
 $.request('onAjax', {
     update: {
