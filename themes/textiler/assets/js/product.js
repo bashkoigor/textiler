e2e91233ce076c1e97b163b995e72cde454a088d
admin <admin@mail.loc>
2022-08-09 19:02:38 +0200

    init offers on product page

--- themes/textiler/assets/js/product.js
+++ themes/textiler/assets/js/product.js
@@ -5,3 +5,20 @@ $.request('Cart::onGetCartData', {
     }
 });
 
+// Event set offer id
+$(document).on('click', '.field-color-picker li span', (e) => {
+    let offerId = $(e.currentTarget).data("offerid");
+    setOffer(offerId);
+});
+
+// Set offer id
+function setOffer(offerId) {
+    $.request('onInit', {
+        data: {'offerId': offerId},
+        update: {
+            'product/card': '.product-wrapper'
+        }
+    });
+}
+
+
